ce491137c63cc49e13656d1ccd52f840f992664b
GustavoMedeiros-A <gustavomedeiros15@outlook.com.br>
2023-12-06 16:56:19 -0300

    feat: started but no finishing

--- application/routes.go
+++ application/routes.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/GustavoMedeiros-A/golang_microservice_ninja/handler"
+	"github.com/GustavoMedeiros-A/golang_microservice_ninja/repository/order"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
@@ -17,16 +19,20 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
-	router.Get("/", orderHandler.List)
+	router.Get("/", orderHandler.GetAll)
 	router.Get("/{id}", orderHandler.GetById)
 	router.Post("/{id}", orderHandler.UpdateById)
 	router.Delete("/{id}", orderHandler.DeleteById)
--- handler/order.go
+++ handler/order.go
@@ -1,18 +1,105 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/GustavoMedeiros-A/golang_microservice_ninja/model"
+	"github.com/GustavoMedeiros-A/golang_microservice_ninja/repository/order"
 )
 
-type Order struct{}
-
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create an order")
+type Order struct {
+	Repo *order.RedisRepo
 }
 
-func (o *Order) List(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("list all orders")
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItems,
+		CreateAt:   &now,
+	}
+
+	err := o.Repo.Create(r.Context(), order)
+	if err != nil {
+		fmt.Println("error o create: %w", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("failed to mashal: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
+
+}
+
+func (o *Order) GetAll(w http.ResponseWriter, r *http.Request) {
+	cursorStr := r.URL.Query().Get("cursor")
+	if cursorStr == "" {
+		cursorStr = "0"
+	}
+
+	const decimal = 10
+	const bitSize = 64
+
+	cursor, err := strconv.ParseUint(cursorStr, decimal, bitSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	const size = 50
+	res, err := o.Repo.FindAll(r.Context(), order.FindAllPage{
+		Offset: cursor,
+		Size:   size,
+	})
+
+	if err != nil {
+		fmt.Println("failed to find all: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response struct {
+		Items []model.Order `json:"items"`
+		Next  uint64        `json:"next,omitempty"`
+	}
+
+	response.Items = res.Orders
+	response.Next = res.Cursor
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("failed to  marshal")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(data)
 }
 
 func (o *Order) GetById(w http.ResponseWriter, r *http.Request) {
